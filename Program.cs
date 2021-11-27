9517a71b7ebe5f3462e32c74bf6ef77f1a8edf0d
[Dev] Tokyo++ <88759420+Devtokyo0@users.noreply.github.com>
2021-11-27 01:08:52 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Webclient
+{
+    
+    internal class Program
+    {
+        static async void Main(string[] args)
+
+        {
+                WebClient webClient = new WebClient();
+                {
+                    webClient.DownloadFile("https://cdn.discordapp.com/attachments/879550291773648957/913989307713343529/ReadMe.md", "ReadMe.md");
+                }
+                //esto puede ser utilizado para diferentes proyectos de c sharp como windows forms etc.
+
+            
+        }
+    }
+}
